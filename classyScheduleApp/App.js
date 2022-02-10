1efe4a54e16cb7b54ab55abea2ac39a5f9202007
moll2143 <moll2143@stthomas.edu>
2022-02-09 21:16:18 -0600

    Added image

--- classyScheduleApp/App.js
+++ classyScheduleApp/App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View,Image, SafeAreaView } from 'react-native';
 
 export default function App() {
+  console.log(require('./assets/favicon.png'))
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Image style={styles.tinyLogo}source={require('./assets/favicon.png')}/>
+      <Image style ={styles.logo} source={{uri: 'https://picsum.photos/200/300',}}/>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0ff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 200,
+    height: 300,
   },
 });
