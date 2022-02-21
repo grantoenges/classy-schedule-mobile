b5740b1fc0e1a0fe630b7bd69be39c51c594af93
moll2143 <moll2143@stthomas.edu>
2022-02-20 18:33:03 -0600

    adding react navigation library

--- classyScheduleApp/App.js
+++ classyScheduleApp/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import{NavigationContainer} from '@react-navigation/native-stack'
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
