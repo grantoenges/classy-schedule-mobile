b9d7b2dcb6482cd0613b5aaccfe8959fb0bb6b27
moll2143 <moll2143@stthomas.edu>
2022-03-02 16:37:18 -0600

    adding storage

--- classyScheduleApp/app/screens/classInputting.js
+++ classyScheduleApp/app/screens/classInputting.js
@@ -3,10 +3,29 @@ import { SafeAreaView, View,Text,StyleSheet } from 'react-native';
 import {Button, Card, TextInput} from 'react-native-paper'
 import {Picker} from '@react-native-picker/picker';
 import { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const ClassInputFun = ({navigation}) => {
+
+const ClassInputFun = () => {
     const [selectedLanguage, setSelectedLanguage] = useState();
-
+    const storeData = async (value) => {
+        try {
+          await AsyncStorage.setItem('@storage_Key', value)
+        } catch (e) {
+          // saving error
+        }
+    }
+    const getData = async () => {
+        try {
+          const value = await AsyncStorage.getItem('@storage_Key')
+          if(value !== null) {
+            // value previously stored
+            alert(value)
+          }
+        } catch(e) {
+          // error reading value
+        }
+    }
   return (
     <SafeAreaView style={styles.container}>
         <Card style={styles.cardStyle}>
@@ -17,8 +36,10 @@ const ClassInputFun = ({navigation}) => {
             <Picker.Item label="Stats" value="STAT" />
             <Picker.Item label="Other option" value="Other" />
             <Picker.Item label="wasd" value="wasd" />
-
             </Picker>
+            <Button onPress={() => storeData("66666666")}>save data </Button>
+            <Button onPress={getData}>retr data </Button>
+
         </Card>
     </SafeAreaView>
  );
@@ -28,7 +49,6 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 24,
-      
     },
     cardStyle:{
       backgroundColor:"powderblue"
