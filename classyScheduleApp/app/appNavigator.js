bcf991bf3d234b4b963ca49f00a484ebbc010d2d
moll2143 <moll2143@stthomas.edu>
2022-03-14 16:22:42 -0500

    upd

--- classyScheduleApp/app/appNavigator.js
+++ classyScheduleApp/app/appNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {createStackNavigator} from '@react-navigation/stack'
+import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import  SignInScreenFun from "./screens/SignInScreen"; 
+import SignInScreenFun from "./screens/SignInScreen";
 import WelcomeScreenFun from "./screens/WelcomeScreen";
 import NewAccountScreen from "./screens/NewAccountScreen";
 import PreferenceFun from "./screens/preferencesScreen";
@@ -12,30 +12,29 @@ import ClassesPTFun from "./screens/classesPreferredToTeach";
 import TimesCTFun from "./screens/timesCantTeach";
 import DaysPrefFun from "./screens/daysPreferred";
 import ClassInputFun from "./screens/classInputting";
-import DbCon from "./sqlRe"; 
 
-const {Navigator, Screen} = createStackNavigator ();
-
+const { Navigator, Screen } = createStackNavigator();
 
 const AppNavigator = () => (
-    <NavigationContainer>
-        <Navigator initialRouteName = "Login">
-            <Screen name = "Welcome" component = {WelcomeScreenFun} options={{headerLeft: (props) => null }}/>
-            <Screen name = "Login" component = {SignInScreenFun}/>
-            <Screen name = "NewAccount" component = {NewAccountScreen}/>
-            <Screen name = 'Preferences' component={PreferenceFun}/>
-            <Screen name = 'Settings' component={SettingFun}/>
-            <Screen name = 'Schedule' component={ScheduleFun}/>
-            <Screen name = 'Classes Can Teach' component={ClassesCTFun}/>
-            <Screen name = 'Classes Preferred Teach' component={ClassesPTFun}/>
-            <Screen name = 'Times Cant Teach' component={TimesCTFun}/>
-            <Screen name = 'Days Prefered Teach' component={DaysPrefFun}/>
-            <Screen name = 'Class Input' component={ClassInputFun}/>
-            <Screen name = 'Database' component={DbCon}/>
-
-
-        </Navigator>
-    </NavigationContainer>
-)
+  <NavigationContainer>
+    <Navigator initialRouteName="Login">
+      <Screen
+        name="Welcome"
+        component={WelcomeScreenFun}
+        options={{ headerLeft: (props) => null }}
+      />
+      <Screen name="Login" component={SignInScreenFun} />
+      <Screen name="NewAccount" component={NewAccountScreen} />
+      <Screen name="Preferences" component={PreferenceFun} />
+      <Screen name="Settings" component={SettingFun} />
+      <Screen name="Schedule" component={ScheduleFun} />
+      <Screen name="Classes Can Teach" component={ClassesCTFun} />
+      <Screen name="Classes Preferred Teach" component={ClassesPTFun} />
+      <Screen name="Times Cant Teach" component={TimesCTFun} />
+      <Screen name="Days Prefered Teach" component={DaysPrefFun} />
+      <Screen name="Class Input" component={ClassInputFun} />
+    </Navigator>
+  </NavigationContainer>
+);
 
 export default AppNavigator;
--- classyScheduleApp/app/screens/WelcomeScreen.js
+++ classyScheduleApp/app/screens/WelcomeScreen.js
@@ -1,63 +1,62 @@
-import React from 'react';
-import { SafeAreaView, View,Text,StyleSheet } from 'react-native';
-import {Button, Card, TextInput} from 'react-native-paper'
-import Teste from '../sqlRe'
-import DbCon from '../sqlRe';
+import React from "react";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { Button, Card, TextInput } from "react-native-paper";
 
-const WelcomeScreenFun = ({navigation}) => {
-     const login = () => navigation.navigate("Login")
-     const prefs = () => navigation.navigate("Preferences")
-     const settings = () => navigation.navigate("Settings")
-     const schedule = () => navigation.navigate("Schedule")
-     const Input = () => navigation.navigate("Class Input")
-     const dbCon = () => navigation.navigate("Database")
-
-
+const WelcomeScreenFun = ({ navigation }) => {
+  const login = () => navigation.navigate("Login");
+  const prefs = () => navigation.navigate("Preferences");
+  const settings = () => navigation.navigate("Settings");
+  const schedule = () => navigation.navigate("Schedule");
+  const Input = () => navigation.navigate("Class Input");
 
   return (
     <SafeAreaView style={styles.container}>
-        <Card style={styles.cardStyle}>
-            <Card.Title title= "Welcome Screen"/>
-        </Card>
-        <Button style={styles.buttonStyle} onPress={login}>Login</Button>
-        <Button style={styles.buttonStyle} onPress={prefs}>Preferences</Button>
-        <Button style={styles.buttonStyle} onPress={settings}>Settings</Button>
-        <Button style={styles.buttonStyle} onPress={schedule}>Schedule</Button>
-        <Button style={styles.buttonStyle} onPress={Input}>Class Input</Button>
-        <Button style={styles.buttonStyle} onPress={dbCon}>Database </Button>
-
-
+      <Card style={styles.cardStyle}>
+        <Card.Title title="Welcome Screen" />
+      </Card>
+      <Button style={styles.buttonStyle} onPress={login}>
+        Login
+      </Button>
+      <Button style={styles.buttonStyle} onPress={prefs}>
+        Preferences
+      </Button>
+      <Button style={styles.buttonStyle} onPress={settings}>
+        Settings
+      </Button>
+      <Button style={styles.buttonStyle} onPress={schedule}>
+        Schedule
+      </Button>
+      <Button style={styles.buttonStyle} onPress={Input}>
+        Class Input
+      </Button>
+      <Button style={styles.buttonStyle}> Dummy button</Button>
     </SafeAreaView>
- );
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 24,
-      
-    },
-    cardStyle:{
-        backgroundColor:"powderblue"
-    },
-    buttonStyle:{
-        backgroundColor :"silver"
-    },
-    title: {
-      marginTop: 16,
-      paddingVertical: 8,
-      borderWidth: 4,
-      borderColor: "#20232a",
-      borderRadius: 6,
-      backgroundColor: "#61dafb",
-      color: "#20232a",
-      textAlign: "center",
-      fontSize: 30,
-      fontWeight: "bold"
-    }
-  });
-  
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  cardStyle: {
+    backgroundColor: "powderblue",
+  },
+  buttonStyle: {
+    backgroundColor: "silver",
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
 
 export default WelcomeScreenFun;
-
-
