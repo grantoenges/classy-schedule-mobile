a890f9e79d32bc49513ed6f6eeebc29177c37722
nils8354 <78751071+nils8354@users.noreply.github.com>
2022-03-29 15:26:44 -0500

    Added comments, added styling to welcome

--- classyScheduleApp/app/screens/WelcomeScreen.js
+++ classyScheduleApp/app/screens/WelcomeScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { SafeAreaView, View, Text,Image, StyleSheet } from "react-native";
-import { Button, Card, TextInput } from "react-native-paper";
+import { SafeAreaView, View, Text,Image, StyleSheet, TouchableOpacity } from "react-native";
+import { Button, Card, TextInput, TouchableRipple } from "react-native-paper";
 
 const WelcomeScreenFun = ({ navigation }) => {
   const login = () => navigation.navigate("Login");
@@ -9,30 +9,42 @@ const WelcomeScreenFun = ({ navigation }) => {
   const schedule = () => navigation.navigate("Schedule");
   const Input = () => navigation.navigate("Class Input");
   const ApiTester = () => navigation.navigate("Api Test");
-
+//Color for react native button text is #6200ed
+//Might need to change textFamily for android in textStyle
   return (
     <SafeAreaView style={styles.container}>
       <Card style={styles.cardStyle}>
-        <Card.Title title="Welcome Screen" />
+        <Card.Title title="Navigation Screen" />
       </Card>
-      <Button mode="outlined" style={styles.buttonStyle} onPress={login}>
-        Login
-      </Button>
-      <Button mode="outlined"  style={styles.buttonStyle} onPress={prefs}>
-        Preferences
-      </Button>
-      <Button mode="outlined"  icon="cog" style={styles.buttonStyle} onPress={settings}>
-        Settings
-      </Button>
-      <Button mode="outlined" style={styles.buttonStyle} onPress={schedule}>
-        Schedule
-      </Button>
-      <Button mode="outlined" style={styles.buttonStyle} onPress={Input}>
-        Class Input
-      </Button>
-      <Button mode="outlined" style={styles.buttonStyle} onPress={ApiTester}>
-        API Test (Will crash)
-      </Button>
+
+      <View style={styles.centerPage}>
+        <View style={styles.viewStyle}>
+          <TouchableOpacity mode="contained"  style={styles.buttonStyle} onPress={login}>
+            <Text style={styles.textStyle}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity mode="contained"  style={styles.buttonStyle} onPress={prefs}>
+            <Text style={styles.textStyle}>Preferences</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.viewStyle}>
+          <TouchableOpacity mode="contained"  icon="cog" style={styles.buttonStyle} onPress={settings}>
+            <Text style={styles.textStyle}>Settings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity mode="contained" style={styles.buttonStyle} onPress={schedule}>
+            <Text style={styles.textStyle}>Schedule</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.viewStyle}>
+          <TouchableOpacity mode="contained" style={styles.buttonStyle} onPress={Input}>
+            <Text style={styles.textStyle}>Class Input</Text>
+          </TouchableOpacity>
+          <TouchableOpacity mode="contained" style={styles.buttonStyle} onPress={ApiTester}>
+            <Text style={styles.textStyle}>API Test</Text> 
+          </TouchableOpacity>
+        </View>
+      </View>
       
     </SafeAreaView>
   );
@@ -42,6 +54,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
+  },
+  centerPage: {
+    marginTop: 40,
+    justifyContent: "center",
+    alignItems:"center"
   },
   tinyLogo:{
     width: 150,
@@ -54,20 +71,41 @@ const styles = StyleSheet.create({
     backgroundColor: "grey",
   },
   buttonStyle: {
-    
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
+    width: "100%",
+    height: "100%",
+    minWidth: 150,
+    minHeight: 150,
+    maxWidth:175,
+    maxHeight: 175,
     textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold",
+    backgroundColor: "#6200ed",
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "white",
+    elevation: 10,
+    justifyContent: "center",
   },
+  textStyle: {
+    
+    //color: "#6200ed",
+    color: "white",
+    justifyContent: "center",
+    alignItems:"center",
+    textAlign: "center",
+    fontWeight: '500',
+    fontSize: 14,
+    fontFamily: "System",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+    marginVertical: 9,
+    marginHorizontal: 16,
+  },
+
+  viewStyle: {
+    flex:-1,
+    flexDirection:"row",
+    marginTop: 10,
+  }
 });
 
 export default WelcomeScreenFun;
