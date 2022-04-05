8f6f468d4a5675ae426b4cbe69a46474d35c37ea
nils8354 <78751071+nils8354@users.noreply.github.com>
2022-04-04 20:43:52 -0500

    Added screen and window dimensions to styling

--- classyScheduleApp/app/screens/WelcomeScreen.js
+++ classyScheduleApp/app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, View, Text,Image, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { SafeAreaView, View, Text,Image, StyleSheet, TouchableOpacity, Alert, ScrollView } from "react-native";
 import { Button, Card, TextInput, TouchableRipple } from "react-native-paper";
 import { MyComponent } from "./Shake";
 import RNShake from 'react-native-shake';
@@ -50,39 +50,36 @@ MyComponent();
 
   return (
     <SafeAreaView style={styles.container}>
-      <Card style={styles.cardStyleWelcome}>
-        <Card.Title title="Navigation Screen" />
-      </Card>
-
-      <View style={styles.centerPageMargin}>
+      
+      <View style={styles.scrollview}>
         <View style={styles.viewStyle}>
-          <TouchableOpacity mode="contained"  style={styles.buttonStyle} onPress={login}>
+          <TouchableOpacity mode="contained"   activeOpacity={0.8} style={styles.buttonStyle} onPress={login}>
             <Text style={styles.textStyle}>Login</Text>
           </TouchableOpacity>
-          <TouchableOpacity mode="contained"  style={styles.buttonStyle} onPress={prefs}>
+          <TouchableOpacity mode="contained"  style={styles.buttonStyle} activeOpacity={0.8} onPress={prefs}>
             <Text style={styles.textStyle}>Preferences</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.viewStyle}>
-          <TouchableOpacity mode="contained"  icon="cog" style={styles.buttonStyle} onPress={settings}>
+          <TouchableOpacity mode="contained" style={styles.buttonStyle} activeOpacity={0.8} onPress={settings}>
             <Text style={styles.textStyle}>Settings</Text>
           </TouchableOpacity>
-          <TouchableOpacity mode="contained" style={styles.buttonStyle} onPress={schedule}>
+          <TouchableOpacity mode="contained" style={styles.buttonStyle} activeOpacity={0.8} onPress={schedule}>
             <Text style={styles.textStyle}>Schedule</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.viewStyle}>
-          <TouchableOpacity mode="contained" style={styles.buttonStyle} onPress={Input}>
+          <TouchableOpacity mode="contained" style={styles.buttonStyle} activeOpacity={0.8} onPress={Input}>
             <Text style={styles.textStyle}>Class Input</Text>
           </TouchableOpacity>
-          <TouchableOpacity mode="contained" style={styles.buttonStyle} onPress={ApiLister}>
+          <TouchableOpacity mode="contained" style={styles.buttonStyle} activeOpacity={0.8} onPress={ApiLister}>
             <Text style={styles.textStyle}>Dynamic checklist List</Text> 
           </TouchableOpacity>
         </View>
         <View style={styles.viewStyle}>
-          <TouchableOpacity mode="contained" style={styles.buttonStyle} onPress={ApiTester}>
+          <TouchableOpacity mode="contained" style={styles.buttonStyle} activeOpacity={0.8} onPress={ApiTester}>
             <Text style={styles.textStyle}>APi test</Text>
           </TouchableOpacity>
         </View>
@@ -93,6 +90,4 @@ MyComponent();
 };
 
 
-
 export default WelcomeScreenFun;
-
