73a2023d8482f502326be1358086af74858c06ea
moll2143 <moll2143@stthomas.edu>
2022-04-20 17:00:35 -0500

    moved auth

--- classyScheduleApp/app/screens/ApiTest.js
+++ classyScheduleApp/app/screens/ApiTest.js
@@ -26,29 +26,33 @@ import {Button, Card, Checkbox, TextInput} from 'react-native-paper'
      try {
        setLoading(true);
        setDataT([]);
-      const response = await fetch('https://capstonedbapi.azurewebsites.net/department-management/departments', {
+      const response = await fetch('https://capstonedbapi.azurewebsites.net/class-management/classes', {
         method: 'GET',
         /*,  Example of how headers look for if people are to take this to use on other parts of the app */ 
         headers: { 
           //Will need the authorization to be a saved string each time we sign in
-          'Authorization': auth
+          'Authorization': AUTH._W
         },
         });
-      const json = await response.json();
+        const json = await response.json();
+
         setDataT((dataT) => [
           ...dataT,
-          ...json.map(({dept_id, dept_name}) => ({
+          ...json.map(({class_num,dept_id, class_name, capacity, credits}) => ({
+            class_num,
             dept_id,
-            dept_name,
+            class_name,
             checked:false
           //
           })),
         ]);
-      console.log(dataT);
+      //console.log(dataT);
       //setData(json);
       } catch (error) {
       console.error(error);
     } finally {
+
+      
       setLoading(false);
     }
   }
@@ -59,22 +63,20 @@ import {Button, Card, Checkbox, TextInput} from 'react-native-paper'
     getJson();
   }, []);
 
-  /*This return is where the actual react part of the app is made and the  */
+  /*This return is where the actual react part of the app is made and the 
+  */
   return (
     <View style={{ flex: 1, padding: 24 }}>      
-      <Button onPress={()=>{console.log('hh')}}>Save Data</Button>
-      {isLoading ? <Button loading ={true} mode="contained"> Loading</Button> : (
-        
+      <Button onPress={()=>{console.log('hh')}} mode="contained" >Save Data</Button>
+      {isLoading ? <Button loading ={true} mode="outlined"> Loading</Button> : (
         <FlatList
           data={dataT}
-          keyExtractor={({ dept_id }) => dept_id}
+          keyExtractor={({ class_num }) => class_num}
           renderItem={({ item }) => (
-              <Checkbox.Item label={item.dept_name} color="green" uncheckedColor="black"status={item.checked? 'checked':'unchecked'} onPress={()=>{item.checked = !item.checked; setDummy(!dummy)}}/>
+              <Checkbox.Item label={ item.dept_id + ": " + item.class_name} color="darkblue" uncheckedColor="black"status={item.checked? 'checked':'unchecked'} onPress={()=>{item.checked = !item.checked; setDummy(!dummy)}}/>
             )}
-        />
-        
+        />   
       )}
-
     </View>
   );
 };
