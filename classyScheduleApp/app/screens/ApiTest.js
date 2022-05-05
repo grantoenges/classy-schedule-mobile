fd23909491d51ebeccc3c4340643ce4413d6d391
moll2143 <moll2143@stthomas.edu>
2022-05-04 19:12:50 -0500

    l

--- classyScheduleApp/app/screens/ApiTest.js
+++ classyScheduleApp/app/screens/ApiTest.js
@@ -5,13 +5,91 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
  const Apites = () => {
+   /*This is a temporary variable that holds the current authorization token to allow for connections with the database */
+
   /*This usestate variable is used as a flag, keeping track of the loading vs not loading of the data*/
   const [isLoading, setLoading] = useState(true);
-  const [dummy, setDummy] = React.useState(false);
-
+  /*This usestate variable is used as a flag, keeping track of the when the page has information changed and will need a reload of the data*/
+  const [dummy, setDummy] = useState(false);
   /*This usestate variable is used as the json data obtained from the api calls storage location*/
-  const [data, setData] = useState([]);
+  const [pref, setPref] = useState([]);
   const [dataT, setDataT] = useState([]);
+
+
+  const seeSelection = async() =>{
+    try{
+     setLoading(true);
+     console.log(pref);
+     console.log(pref.find(element => element.class_num === 12345).prefer_to_teach);
+     }
+       catch (error) {
+         console.error(error);
+       } finally {
+         setLoading(false);
+       }
+  }
+
+ const sendSelection = async() =>{
+   try{
+    setLoading(true);
+    const auth = await AsyncStorage.getItem('Auth');
+    const id = await AsyncStorage.getItem('UserId');
+
+    const response = await fetch('https://capstonedbapi.azurewebsites.net/preference-management/class-preferences/prefer-to-teach/save/'+id, {
+      method: 'POST',
+      /*,  Example of how headers look for if people are to take this to use on other parts of the app */ 
+      headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': auth//'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJuYmYiOjE2NDkxMDYwNTEsImV4cCI6MTY0OTcxMDg1MSwiaWF0IjoxNjQ5MTA2MDUxfQ.FlDyEzy_0dDG-VM5oIvvIWYI2Zo7MMUcS9KnEoiJ2_s'
+      },
+        body: JSON.stringify(dataT)
+      });
+      const json = await response.json();
+    
+    alert(json);
+    }
+      catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+ }
+
+ const getPreferencesJson = async () => {
+  try {
+    setLoading(true);
+    setPref([]);
+    const auth = await AsyncStorage.getItem('Auth');
+    const id = await AsyncStorage.getItem('UserId');
+
+   const response = await fetch('https://capstonedbapi.azurewebsites.net/preference-management/class-preferences/prefer-to-teach/'+id, {
+     method: 'GET',
+     /*,  Example of how headers look for if people are to take this to use on other parts of the app */ 
+     headers: { 
+       //Will need the authorization to be a saved string each time we sign in
+       'Authorization': auth
+     },
+     });
+   const json = await response.json();
+   /*This mapping function allows us to tag an extra variable to the data received that tells us if the class is selected */
+     setPref((pref) => [
+       ...pref,
+       ...json.map(({class_num,dept_id,is_lab, class_name, prefer_to_teach}) => ({
+         class_num,
+         dept_id,
+         is_lab,
+         class_name,
+         prefer_to_teach
+       })),
+     ]);
+     console.log(json);
+   } catch (error) {
+   console.error(error);
+ } finally {
+   setLoading(false);
+ }
+}
   /*
   getJson's purpose is to make a call to the API point and set our usestate variable to the data that 
   should be returned while also updating the isLoading variable to reflect the loading status 
@@ -25,8 +103,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
   const getJson = async () => {
      try {
        setLoading(true);
-       const auth = await AsyncStorage.getItem('Auth');
        setDataT([]);
+       const auth = await AsyncStorage.getItem('Auth');
+
+       console.log('Current auth token', auth);
       const response = await fetch('https://capstonedbapi.azurewebsites.net/class-management/classes', {
         method: 'GET',
         /*,  Example of how headers look for if people are to take this to use on other parts of the app */ 
@@ -35,16 +115,16 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
           'Authorization': auth
         },
         });
-        const json = await response.json();
-
+      const json = await response.json();
+      /*This mapping function allows us to tag an extra variable to the data received that tells us if the class is selected */
         setDataT((dataT) => [
           ...dataT,
-          ...json.map(({class_num,dept_id, class_name, capacity, credits}) => ({
+          ...json.map(({class_num,dept_id, class_name,is_lab, capacity, credits}) => ({
             class_num,
             dept_id,
             class_name,
-            checked:false
-          //
+            is_lab,
+            prefer_to_teach:false //pref.find(element => element.class_num === 12345)? true: false
           })),
         ]);
       } catch (error) {
@@ -57,54 +137,27 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
   /*useEffect is a react native hook that allows us to get to using our usestate variables and allowing
   for the dynamic rendering of that data onto the screen. This useeffect for example calls our getJson method */
   useEffect(() => {
+    //getAuth();
+    getPreferencesJson();
     getJson();
   }, []);
 
-  /*This return is where the actual react part of the app is made and the 
-  */
+  /*This return is where the actual react part of the app is made and the data will be displayed for the user  */
   return (
     <View style = {{ flex: 1, padding: 24 }}>      
-      <Button onPress = {()=>{console.log(dataT)}} mode = "contained" >Save Data</Button>
-      {isLoading ? <Button loading = {true} mode = "outlined"> Loading</Button> : (
+      <Button onPress = {sendSelection} mode = "contained" >Save Data</Button>
+      {isLoading ? <Button loading = {true} mode = "outlined" onPress={seeSelection}> Loading</Button> : (
         <FlatList
-          data={dataT}
-          keyExtractor = {({ class_num }) => class_num}
+          data = {dataT}
+          keyExtractor = {({ class_num}) => (class_num) }
           renderItem = {({ item }) => (
-              <Checkbox.Item label = { item.dept_id + ": " + item.class_name} color = "darkblue" uncheckedColor = "black" status = {item.checked? 'checked':'unchecked'} onPress = {()=>{item.checked = !item.checked; setDummy(!dummy)}}/>
+              <Checkbox.Item label = {item.class_name} color = "darkblue" uncheckedColor = "black" status = {item.prefer_to_teach? 'checked':'unchecked'} onPress = {()=>{item.prefer_to_teach = !item.prefer_to_teach; setDummy(!dummy)}}/>
             )}
         />   
       )}
+        <Button mode="contained" onPress={seeSelection} >see data </Button>
+
     </View>
   );
 };
-
-/*This is the pages chouce for design of different containers. Similar syntax to CSS. Subject to drastic change. */
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 24,
-    },
-    cardStyle: {
-      backgroundColor: "powderblue",
-    },
-    buttonStyle: {
-      backgroundColor: "silver",
-    },
-    label: {
-      color: "black",
-    },
-    title: {
-      marginTop: 16,
-      paddingVertical: 8,
-      borderWidth: 4,
-      borderColor: "#20232a",
-      borderRadius: 6,
-      backgroundColor: "#61dafb",
-      color: "#20232a",
-      textAlign: "center",
-      fontSize: 30,
-      fontWeight: "bold",
-    },
-  });
-
 export default Apites;
