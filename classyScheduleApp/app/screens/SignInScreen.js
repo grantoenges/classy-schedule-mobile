4e25b7faf169f601adca021258e50eb7fe62ae4a
Grant <toen1806@stthomas.edu>
2022-05-09 16:49:11 -0500

    bio auth works

--- classyScheduleApp/app/screens/SignInScreen.js
+++ classyScheduleApp/app/screens/SignInScreen.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import {
   SafeAreaView,
   View,
@@ -12,6 +12,12 @@ import { Button, TextInput, Title, useTheme} from "react-native-paper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getAuthorization, signIn } from "../databaseService";
 import styles from '../Style'
+import { 
+  hasHardwareAsync,
+  isEnrolledAsync,
+  authenticateAsync 
+} from 'expo-local-authentication';
+import * as SecureStore from 'expo-secure-store';
 
 //Function that validates email
 function emailValidator(email) {
@@ -34,9 +40,23 @@ function passwordValidator(password) {
   return "";
 }
 
+//function that securely stores the username/password
+async function getSecureValue(key) {
+  let result = await SecureStore.getItemAsync(key);
+  if (result) {
+    alert("Here's your value \n" + result);
+  } else {
+    alert('No values stored under that key.');
+  }
+}
+
+async function saveSecureValue(key, value) {
+  await SecureStore.setItemAsync(key, value);
+}
+
+
 const SignInScreenFun = ({ navigation }) => {
-  global.AUTH = signIn();
-  //global.AUTH = AUTH._W
+
   const newAccount = () => navigation.navigate("New Account");
   let value = value || "";
   //let styles = COLORSCHEME[0] ==='dark' ? darkStyles : lightStyles;
@@ -45,9 +65,51 @@ const SignInScreenFun = ({ navigation }) => {
   const [password, setPassword] = useState({ value: "", error: "" });
   
 
+  //when biometric login is selected 
+  const onBiometricsPressed = async () => {
+    //check if biometrics are available on the device
+    const compatible = await hasHardwareAsync()
+    if(compatible){
+      //check to see if biometrics are saved
+      const savedBiometrics = await isEnrolledAsync();
+      //biometrics are saved on device
+      if(savedBiometrics) {
+        const bioAuthenticate = await authenticateAsync();
+        if(bioAuthenticate.success){
+          //user is authenticated, get username and password
+          const username = await SecureStore.getItemAsync('username');
+          const password = await SecureStore.getItemAsync('password');
+          //send the sign in request and change the page the user is on
+          global.TEMP = signIn(username, password).then((response) => {
+            if(response.token){
+              //succesful login
+              global.AUTH = response.token;
+              global.USERNAME = response.username;
+              global.ROLE = response.user_role;
+              global.USERID = response.user_id;
+              setLoading(false);
+              navigation.navigate("Welcome")
+            } else {
+              //unsucessful login
+              alert(response.message);
+              setLoading(false);
+            }
+          });
+        } else {
+          alert('could not Authenticate user');
+        }
+      } else {
+        alert('No Saved Biometrics on device');
+        return;
+      }
+    } else {
+      alert('device does not support biometrics');
+      return;
+    }
+  }
+
   //When login is pressed this will run error checkers and navigation if no problems
   const onLoginPressed = () => {
-
     setLoading(true);
     const emailError = emailValidator(email.value);
     const passwordError = passwordValidator(password.value);
@@ -59,10 +121,10 @@ const SignInScreenFun = ({ navigation }) => {
     } else {
       //send sign in request 
       global.TEMP = signIn(email.value, password.value).then((response) => {
-        console.log('response', response);
         if(response.token){
           //succesful login
-          console.log('succesful login');
+          saveSecureValue('username', email.value);
+          saveSecureValue('password', password.value);
           global.AUTH = response.token;
           global.USERNAME = response.username;
           global.ROLE = response.user_role;
@@ -116,6 +178,8 @@ const SignInScreenFun = ({ navigation }) => {
       />
       {/* Login button */}
       <Button mode="contained" style={styles.generalButtonContained}  loading ={isLoading} onPress={() => onLoginPressed()}>Login</Button>
+      {/* Biometrics button*/}
+      <Button mode="text" style={styles.generalButtonContained}  loading ={isLoading} onPress={() => onBiometricsPressed()}>Biometric Login</Button>
       {/* Account Creation */}
       <Button mode="text" style={[styles.generalButton, {marginBottom: 5}]}  icon="account-plus"  onPress={newAccount}>Create Account</Button>
       {/* Forgot password */}
